8f452538b07278f7654abe6749c82482f1e10adc
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-08-22 16:39:33 -0300

    Created the index.html page and styled it

---
+++ style/js/script.js
@@ -0,0 +1,11 @@
+ //FUNÇÃO DE FECHAR O MENU
+ $(".btn-close").click(function(){
+    $(".itens-menu").hide();
+    $(".btn-menu").show();
+});
+
+//FUNÇÃO DE ABRIR O MENU
+$(".btn-menu").click(function(){
+    $(".itens-menu").show();
+    $(".btn-menu").hide();
+});
