a4ead3778d1e911b2735adff916cf1012bf02417
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-08-24 17:32:28 -0300

    implemented function of the buttons to close and maximize

--- style/js/script.js
+++ style/js/script.js
@@ -10,4 +10,29 @@ $(".btn-menu").click(function(){
     $(".btn-menu").hide();
 });
 
+// MODAL | IFRAME
+const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        const card_id = card.getAttribute("id");
+        modalOverlay.classList.add("active");
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${card_id}`;
+        console.log(`https://app.rocketseat.com.br/${card_id}`);
+    })
+}
+
+document.querySelector(".close-model").addEventListener("click", function(){
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = "";
+
+})
+const model = document.querySelector(".model");
+const iframe = document.querySelector("iframe");
+const max = document.querySelector(".max-model");
+max.addEventListener("click",function(){
+    model.classList.toggle("maximizado");
+    iframe.classList.toggle("maximizado");
+})
+
